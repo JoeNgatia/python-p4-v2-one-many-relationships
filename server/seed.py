6cccf17e64e1b8140770f8bb30ab52e148754ec0
joengatia <ngatiajoe2@gmail.com>
2024-10-22 21:25:15 +0300

    intial commit

--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,63 @@
-#!/usr/bin/env python3
-# server/seed.py
+# server/models.py
 
-import datetime
-from app import app
-from models import db, Employee, Review, Onboarding
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-with app.app_context():
-    # Delete all rows in tables
-    Employee.query.delete()
-    Review.query.delete()
-    Onboarding.query.delete()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-    # Add model instances to database
-    uri = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
-    tristan = Employee(name="Tristan Tal", hire_date=datetime.datetime(2020, 1, 30))
-    db.session.add_all([uri, tristan])
-    db.session.commit()
+db = SQLAlchemy(metadata=metadata)
 
-    # 1..many relationship between Employee and Review
-    uri_2023 = Review(year=2023, summary="Great web developer!")
-    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work")
-    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches")
-    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker")
-    db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
-    db.session.commit()
 
-    # 1..1 relationship between Employee and Onboarding
-    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27))
-    tristan_onboarding = Onboarding(
-        orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True
-    )
-    db.session.add_all([uri_onboarding, tristan_onboarding])
-    db.session.commit()
+class Employee(db.Model):
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    hire_date = db.Column(db.Date)
+
+    # Relationship mapping the employee to related reviews
+    reviews = db.relationship(
+        'Review', back_populates="employee", cascade='all, delete-orphan')
+
+    # Relationship mapping employee to related onboarding
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
+
+
+class Onboarding(db.Model):
+    __tablename__ = 'onboardings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    orientation = db.Column(db.DateTime)
+    forms_complete = db.Column(db.Boolean, default=False)
+
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
+
+    def __repr__(self):
+        return f'<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer)
+    summary = db.Column(db.String)
+
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping the review to related employee
+    employee = db.relationship('Employee', back_populates="reviews")
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.year}, {self.summary}>'
